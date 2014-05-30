9c3ca90ba91d390a613bec504f321ceeb15ba209
ks <ks@ks-work.(none)>
2014-05-30 13:51:44 +0400

    clojure пока мне неподвластен - ниасилил :[

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to gd
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/gd/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gd.core-test
-  (:use clojure.test
-        gd.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
